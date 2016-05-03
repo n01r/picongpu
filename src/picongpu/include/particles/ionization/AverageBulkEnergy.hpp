a84ac7ea029c73767e603c27cef6cda2ea6765fd
Marco Garten <m.garten@hzdr.de>
2016-05-03 17:32:12 +0200

    Save before deletion of plugin

---
+++ src/picongpu/include/particles/ionization/AverageBulkEnergy.hpp
@@ -0,0 +1,78 @@
+/**
+ * Copyright 2016 Marco Garten, Axel Huebl, Heiko Burau, Rene Widera,
+ *                Felix Schmitt
+ *
+ * This file is part of PIConGPU.
+ *
+ * PIConGPU is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PIConGPU is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PIConGPU.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+
+#pragma once
+
+
+
+namespace picongpu
+{
+namespace particles
+{
+namespace ionization
+{
+
+
+/* functor to calculate average bulk energy */
+template<typename T_SpeciesName, typename T_Area>
+struct ComputeAverageBulkEnergy
+{
+    typedef typename T_SpeciesName::type SpeciesName;
+    static const uint32_t area = T_Area::value;
+
+    HINLINE void operator()( FieldTmp* fieldTmp,
+                             const uint32_t currentStep) const
+    {
+        DataConnector &dc = Environment<>::get().DataConnector();
+
+        /* load species without copying the particle data to the host */
+        SpeciesName* speciesTmp = &(dc.getData<SpeciesName >(SpeciesName::FrameType::getName(), true));
+
+        /* run algorithm */
+        typedef typename particleToGrid::CreateAveragedEnergyOperation<SpeciesName>::type::Solver AverageBulkEnergySolver;
+        fieldTmp->computeValue < area, AverageBulkEnergySolver > (*speciesTmp, currentStep);
+        dc.releaseData(SpeciesName::FrameType::getName());
+    }
+};
+
+typedef SuperCellSize BlockDim;
+
+DataConnector &dc = Environment<>::get().DataConnector();
+
+/* load FieldTmp without copying data to host */
+FieldTmp* fieldTmp = &(dc.getData<FieldTmp > (FieldTmp::getName(), true));
+/* reset average bulk energy values to zero */
+fieldTmp->getGridBuffer().getDeviceBuffer().setValue(FieldTmp::ValueType(0.0));
+
+/* calculate and add the average bulk energy from the electron species in FieldTmp */
+/* @todo filter for species and make the filter parameters user-customizable */
+ForEach<Species1, picongpu::particles::ionization::ComputeAverageBulkEnergy<bmpl::_1,bmpl::int_<CORE + BORDER> >, MakeIdentifier<bmpl::_1> > computeAverageBulkEnergy;
+computeAverageBulkEnergy(forward(fieldTmp), currentStep);
+
+/* add results of all species that are still in GUARD to next GPUs BORDER */
+EventTask fieldTmpEvent = fieldTmp->asyncCommunication(__getTransactionEvent());
+__setTransactionEvent(fieldTmpEvent);
+
+} //namespace ionization
+} //namespace particles
+} // namespace picongpu
---
+++ src/picongpu/include/particles/particleToGrid/ComputeAveragedGridValuePerFrame.hpp
@@ -0,0 +1,131 @@
+/**
+ * Copyright 2013-2016 Marco Garten, Axel Huebl, Heiko Burau, Rene Widera
+ *
+ * This file is part of PIConGPU.
+ *
+ * PIConGPU is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PIConGPU is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PIConGPU.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+
+#pragma once
+
+#include "simulation_defines.hpp"
+#include "pmacc_types.hpp"
+
+#include "math/Vector.hpp"
+#include "particles/particleToGrid/ComputeAverageGridValuePerFrame.def"
+#include "particles/particleToGrid/derivedAttributes/DerivedAttributes.hpp"
+
+#include "algorithms/Gamma.hpp"
+
+#include <vector>
+
+namespace picongpu
+{
+namespace particleToGrid
+{
+
+template<class T_ParticleShape, class T_DerivedAttribute>
+HDINLINE float1_64
+ComputeAverageGridValuePerFrame<T_ParticleShape, T_DerivedAttribute>::getUnit() const
+{
+    return T_DerivedAttribute().getUnit();
+}
+
+template<class T_ParticleShape, class T_DerivedAttribute>
+HDINLINE std::vector<float_64>
+ComputeAverageGridValuePerFrame<T_ParticleShape, T_DerivedAttribute>::getUnitDimension() const
+{
+    return T_DerivedAttribute().getUnitDimension();
+}
+
+template<class T_ParticleShape, class T_DerivedAttribute>
+HINLINE std::string
+ComputeAverageGridValuePerFrame<T_ParticleShape, T_DerivedAttribute>::getName() const
+{
+    return T_DerivedAttribute().getName();
+}
+/** Functor implementation
+ *
+ * \tparam T_ParticleShape type of particle shape
+ * \tparam
+ */
+template<class T_ParticleShape, class T_DerivedAttribute>
+template<class FrameType, class TVecSuperCell, class BoxTmpResult, class BoxTmpCounter >
+DINLINE void
+ComputeAverageGridValuePerFrame<T_ParticleShape, T_DerivedAttribute>::operator()
+(FrameType& frame,
+ const int localIdx,
+ const TVecSuperCell superCell,
+ BoxTmpResult& tmpResultBox,
+ BoxTmpCounter& tmpCounterBox)
+{
+    /* \todo in the future and if useful, the functor can be a parameter */
+    T_DerivedAttribute particleAttribute;
+
+    PMACC_AUTO(particle, frame[localIdx]);
+
+    /* particle attributes: position and generic, derived attribute */
+    const floatD_X pos = particle[position_];
+    const PMACC_AUTO(particleAttr, particleAttribute( particle ));
+
+    /** Shift to the cell the particle belongs to */
+    const int particleCellIdx = particle[localCellIdx_];
+    const DataSpace<TVecSuperCell::dim> localCell(
+        DataSpaceOperations<TVecSuperCell::dim>::map( superCell, particleCellIdx )
+    );
+    PMACC_AUTO(fieldTmpShiftToParticle, tmpBox.shift(localCell));
+
+    /** loop around local super cell position (regarding shape)
+     * \todo take care of non-yee cells
+     */
+    const DataSpace<simDim> lowMargin(LowerMargin().toRT());
+    const DataSpace<simDim> upMargin(UpperMargin().toRT());
+
+    const DataSpace<simDim> marginSpace(upMargin + lowMargin + 1);
+
+    const int numWriteCells = marginSpace.productOfComponents();
+
+    __syncthreads();
+
+    for (int i = 0; i < numWriteCells; ++i)
+    {
+        /* multidimensionalIndex is only positive: defined range = [0,LowerMargin+UpperMargin]*/
+        const DataSpace<simDim> multidimensionalIndex = DataSpaceOperations<simDim>::map(marginSpace, i);
+        /* transform coordinate system that it is relative to particle
+         * offsetToBaseCell defined range = [-LowerMargin,UpperMargin]
+         */
+        const DataSpace<simDim> offsetToBaseCell = multidimensionalIndex - lowMargin;
+        floatD_X assign;
+        for (uint32_t d = 0; d < simDim; ++d)
+            assign[d] = AssignmentFunction()(float_X(offsetToBaseCell[d]) - pos[d]);
+
+
+        /* calculate overlap of particle shape with local cell */
+        const float_X assignComb = assign.productOfComponents();
+
+        /** add the doubly weighted attribute to the FieldTmp-value of the local cell
+         * 1st: value is weighted with the overlap of particle shape and cell
+         * 2nd: value is multiplied by the particle weighting for later averaging
+         * over the "real" particle number
+         */
+        atomicAddWrapper(&(fieldTmpShiftToParticle(offsetToBaseCell).x()),
+                         assignComb * particleAttr * particle[weighting_]);
+    }
+}
+
+} // namespace particleToGrid
+} // namespace picongpu
